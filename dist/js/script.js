a8768355078630d04d8e48b8e986db50993e3c37
VitaliiBaryliuk <vitaliyua912@gmail.com>
2018-11-19 17:27:14 +0200

    files uploaded

---
+++ dist/js/script.js
@@ -0,0 +1,50 @@
+(function () {
+  const overlay = document.createElement('div');
+  overlay.classList.add('back-color', 'hide');
+  document.body.appendChild(overlay);
+
+  const closeItem = document.createElement('a');
+  closeItem.classList.add('close-item', 'hide');
+  closeItem.innerHTML = '<img src="images/icon-popup-close.svg" alt="">';
+  closeItem.dataset.modal = 'close';
+  document.body.insertBefore(closeItem, document.body.firstChild);
+
+  const dataModal = document.querySelectorAll('[data-modal]');
+  const modalLinks = document.querySelectorAll('.modal-link');
+  const buttons = document.querySelector('.buttons');
+  // console.log(buttons);
+
+  function makeStyle(hide, modalElem, bagrElem) {
+    if (hide === true) {
+      modalElem.classList.add('hide');
+      bagrElem.classList.add('hide');
+      buttons.classList.remove('hide');
+    } else {
+      modalElem.classList.remove('hide');
+      bagrElem.classList.remove('hide');
+      buttons.classList.add('hide');
+    }
+  }
+
+  modalLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      currentWindow = document.querySelector(`div[data-contant="${event.target.dataset.link}"]`);
+      currentWindow.insertBefore(closeItem, currentWindow.firstChild);
+      closeItem.classList.remove('hide');
+      makeStyle(false, currentWindow, overlay);
+    });
+  });
+
+  overlay.addEventListener('click', (event) => {
+    if (event.target.classList === overlay.classList) {
+      makeStyle(true, currentWindow, overlay);
+    }
+  });
+
+  for (let i = 0; i < dataModal.length; i += 1) {
+    dataModal[i].addEventListener('click', (event) => {
+      makeStyle(true, currentWindow, overlay);
+      event.target.dataset.modal === 'alert' ? setTimeout("alert('Some mesage')", 100) : console.log();
+    });
+  }
+}());
